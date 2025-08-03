7a9b6274e2762955e4f0dc5f39ce6b030922d5ed
vitalivo <vitalivo@gmail.com>
2025-08-03 20:13:48 +0300

    Fix healthcheck path in railway.toml

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-producti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 DJANGO_APPS = [
