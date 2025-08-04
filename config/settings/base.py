fd402836d0ae99084c1cd72d208c21945064719d
vitalivo <vitalivo@gmail.com>
2025-08-04 08:48:59 +0300

    deploy push now

--- config/settings/base.py
+++ config/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
